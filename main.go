b210a8f4d7b7bec5478e96243f1957bfa5841030
Andry <slimfy1@gmail.com>
2026-01-22 21:43:04 +0300

    Add currency calculator

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Main body
+	// Current price
+	const usdToEur float64 = 0.85
+	const usdToRub float64 = 76.0382
+
+	fmt.Println("Enter the amount EUR you want to convert to RUB:")
+	var amount float64
+	fmt.Scanln(&amount)
+
+	eurToRub := (amount * usdToRub) / usdToEur
+	fmt.Printf("Your money: %v", eurToRub)
+}
