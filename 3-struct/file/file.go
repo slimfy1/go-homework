de3be9752a16310208d12e8cfd3fcf68cb55ee94
Andry <slimfy1@gmail.com>
2026-02-08 14:23:04 +0300

    Add file work realisation

--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -1,1 +1,21 @@
 package file
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+//Чтение любого файла
+//Проверка что это json расширение файла
+
+func readFile(filename string) (string, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	if filepath.Ext(filename) != ".json" {
+		return "", errors.New("invalid file extension")
+	}
+	return string(content), nil
+}
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,1 +1,30 @@
 package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+//Сохранение bin в виде json в локальном файле
+//Чтение списка bin в виде json из локального файла
+
+func saveBin(bin []byte, filename string) error {
+	json, err := json.Marshal(bin)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, json, 0644)
+}
+
+func readBins(filename string) ([]byte, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	if filepath.Ext(filename) != ".json" {
+		return nil, errors.New("invalid file extension")
+	}
+	return content, nil
+}
